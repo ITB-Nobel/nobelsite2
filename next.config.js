e66468eba0fe74bcf3b21edf3f34aa348739509e
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2023-11-11 23:54:43 +0800

    downgrade nextjs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     transpilePackages: ['lucide-react'],
     output: "export",
     images:{
+        unoptimized: true,
         remotePatterns:[{
             protocol: 'https',
             hostname: 'nobel.nobelcodelabs.com'
