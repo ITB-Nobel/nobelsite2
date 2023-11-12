8c9c8e8750dec7773cc88099e4ca893c326a5839
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2023-11-12 14:28:03 +0800

    minor chage

--- src/app/jurusan/[id]/components/TabDosenJurusan.tsx
+++ src/app/jurusan/[id]/components/TabDosenJurusan.tsx
@@ -7,16 +7,15 @@ import {Dosen} from "@/lib/types";
 
 const TabDosenJurusan = ({idProdi}: { idProdi: string }) => {
     const {data} = useSWR<{ acf: Dosen }[]>(`dosen-prodi-${idProdi}`, () => fetcherAcf('dosen'))
-    console.log(data, 'dosen-prodi')
     return <section className={"py-12 grid grid-cols-3 text-left w-full"}>
         {
             data && data?.map((item, index) => <StaffDialog
-                id={index}
-                name={item.acf.name}
                 position={item.acf.position}
                 image={item.acf.photo?.url}
                 key={index}
                 description={item.acf.description}
+                group={""}
+                title={item.acf.name}
             />)
         }
     </section>
