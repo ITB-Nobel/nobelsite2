2840b57d9f9e2851074e0fe18ccdb54921352e7c
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2023-11-12 15:37:41 +0800

    change build method

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ['lucide-react'],
-    output: "export",
+    // output: "export",
     images:{
         unoptimized: true,
         remotePatterns:[{
