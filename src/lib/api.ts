3089f5216ed0de8255b6404c4b6cf11eecf953a5
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-12-07 16:08:37 +0800

    disable axios cache

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
 
 export async function fetcher<T>(link: string) {
-    const res: { data: T } = await axios.get(`https://nobel.nobelcodelabs.com/wp-json/wp/v2/${link}`);
+    const res: { data: T } = await axios.get(`https://nobel.nobelcodelabs.com/wp-json/wp/v2/${link}`, {
+        headers: {
+            'Cache-Control': 'no-cache',
+        }
+    });
     return res.data;
 }
 
 export async function fetcherAcf<T>(link: string) {
-    const res: { data: T } = await axios.get(`https://nobel.nobelcodelabs.com/wp-json/acf/v3/${link}`);
+    const res: { data: T } = await axios.get(`https://nobel.nobelcodelabs.com/wp-json/acf/v3/${link}`, {
+        headers: {
+            'Cache-Control': 'no-cache',
+        }
+    });
     return res.data;
 }
 
