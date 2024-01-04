b870f84254485be35f29c990bc2ffff6614604f9
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-01-04 15:21:08 +0800

    feat: add foreign application form

---
+++ src/app/application/page.tsx
@@ -0,0 +1,197 @@
+"use client"
+
+import GeneralLayout from "@/components/layout/GeneralLayout";
+import FormInput from "@/components/form/form-input";
+import {SubmitHandler, useForm} from "react-hook-form";
+import * as zod from 'zod'
+import {zodResolver} from "@hookform/resolvers/zod"
+import {Form} from "@/components/Form";
+import {Button} from "@/components/Button";
+
+{/*
+    Upload Attached Document :
+    - Academic transcripts and high school / MK,  diploma, or equivalent
+    - Certificate of academic achievement and/or non-academic achievement (if Any)
+    - Maximum age 35 years (Proved by Birth Certificate)
+    - Motivation Letter (Ms. Word or PDF)
+    - Passport (min. 30 months after entering college)
+    - Medical certificate
+    - Passport-sized photo
+    - English Proficiency Certificate: TOEFL/TOEIC/IELTS or other valid certificate)
+*/
+}
+
+const formSchema = zod.object({
+    name: zod.string(),
+    birthday: zod.string(),
+    country: zod.string(),
+    identity: zod.string(),
+    identity_number: zod.number(),
+    expiration_date: zod.date(),
+    faculty_objectives: zod.string(),
+    study_program: zod.string(),
+    phone: zod.number(),
+    transcript: zod.string(),
+    certificate:zod.string(),
+    certificate_birthday:zod.string(),
+    motivation_letter:zod.string(),
+    passport:zod.string(),
+    medical_certificate:zod.string(),
+    passport_photo:zod.string(),
+    english_certificate:zod.string(),
+})
+export type FormData = zod.infer<typeof formSchema>
+export default function ApplicationPage() {
+    const form = useForm<FormData>({
+        resolver: zodResolver(formSchema)
+    })
+    const handleFormSubmit: SubmitHandler<FormData> = (data) => {
+        console.log(data)
+    }
+
+    return <GeneralLayout featuredTitle={"APPLICATION FORM"}>
+        <main className={"bg-slate-50"}>
+            <section className={" mx-auto py-12 items-center container"}>
+                <h2 className={"my-8 font-bold text-2xl"}>FOREIGN STUDENT APPLICATION FORM</h2>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleFormSubmit)} >
+                        <div className={"grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-2"} >
+                        <FormInput
+                            control={form.control}
+                            label={"NAME"}
+                            name={"name"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"PLACE AND DATE OF BIRTH"}
+                            name={"birthday"}
+                            type={"date"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"COUNTRY OF ORIGIN"}
+                            name={"country"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"IDENTITY CARD"}
+                            name={"identity"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"IDENTITY CARD NUMBER"}
+                            name={"identity_number"}
+                            type={"number"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"EXPIRATION DATE"}
+                            name={"expiration_date"}
+                            type={"date"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"FACULTY OBJECTIVES"}
+                            name={"faculty_objectives"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"STUDY PROGRAM"}
+                            name={"study_program"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"PHONE NUMBER/WA"}
+                            name={"phone"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Academic transcripts and high school / MK,  diploma, or equivalent"}
+                            name={"transcript"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Certificate of academic achievement and/or non-academic achievement (if Any)"}
+                            name={"certificate"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Maximum age 35 years (Proved by Birth Certificate)"}
+                            name={"certificate_birthday"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Motivation Letter (Ms. Word or PDF)"}
+                            name={"motivation_letter"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Passport (min. 30 months after entering college)"}
+                            name={"passport"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Medical certificate"}
+                            name={"medical_certificate"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"Passport-sized photo"}
+                            name={"passport_photo"}
+                            type={"file"}
+                            required={true}
+                        />
+
+                        <FormInput
+                            control={form.control}
+                            label={"English Proficiency Certificate: TOEFL/TOEIC/IELTS or other valid certificate)"}
+                            name={"english_certificate"}
+                            type={"file"}
+                            required={true}
+                        />
+                        </div>
+                        <Button type={"submit"} className={"mt-4 w-24 text-white"}>Submit</Button>
+                    </form>
+                </Form>
+            </section>
+
+        </main>
+
+    </GeneralLayout>
+}
---
+++ src/components/form/form-input.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/Form";
+import {Input} from "@/components/input";
+import {Control, FieldValues} from "react-hook-form";
+
+
+type FormInputType = {
+    control: Control<any>,
+    required?: boolean,
+    description?: string,
+    placeholder?: string,
+    label: string,
+    name: string,
+    type?: "text" | "number" | "email" | "date" | "file"
+}
+export default function FormInput({required = false,control, description, placeholder, label, name, type = 'text'}: FormInputType) {
+    return <FormField
+        control={control}
+        name={name}
+        render={({field}) => (
+            <FormItem>
+                <FormLabel>{label} {required && <span className={"ml-1 text-red-700"}>*</span>} </FormLabel>
+                <FormControl>
+                    <Input placeholder={placeholder} {...field} type={type}/>
+                </FormControl>
+                {description && <FormDescription>{description}</FormDescription>}
+                <FormMessage/>
+            </FormItem>
+        )}
+    />
+}
