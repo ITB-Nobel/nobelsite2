5b0535fdba3d7add1c337a2aa35c7c2bb4e2a8d3
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-01-04 16:00:55 +0800

    style: change upload application form position

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -32,13 +32,13 @@ const formSchema = zod.object({
     study_program: zod.string(),
     phone: zod.string(),
     transcript: zod.string(),
-    certificate:zod.string(),
-    certificate_birthday:zod.string(),
-    motivation_letter:zod.string(),
-    passport:zod.string(),
-    medical_certificate:zod.string(),
-    passport_photo:zod.string(),
-    english_certificate:zod.string(),
+    certificate: zod.string(),
+    certificate_birthday: zod.string(),
+    motivation_letter: zod.string(),
+    passport: zod.string(),
+    medical_certificate: zod.string(),
+    passport_photo: zod.string(),
+    english_certificate: zod.string(),
 })
 export type FormData = zod.infer<typeof formSchema>
 export default function ApplicationPage() {
@@ -52,140 +52,145 @@ export default function ApplicationPage() {
     return <GeneralLayout featuredTitle={"APPLICATION FORM"}>
         <main className={"bg-slate-50"}>
             <section className={" mx-auto py-12 items-center container"}>
-                <h2 className={"my-8 font-bold text-2xl"}>FOREIGN STUDENT APPLICATION FORM</h2>
+
                 <Form {...form}>
-                    <form onSubmit={form.handleSubmit(handleFormSubmit)} >
-                        <div className={"grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-2"} >
-                        <FormInput
-                            control={form.control}
-                            label={"NAME"}
-                            name={"name"}
-                            required={true}
-                        />
+                    <form onSubmit={form.handleSubmit(handleFormSubmit)}>
+                        <h2 className={"my-8 font-bold text-2xl"}>FOREIGN STUDENT APPLICATION FORM</h2>
+                        <div className={"grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-2"}>
+                                <FormInput
+                                    control={form.control}
+                                    label={"NAME"}
+                                    name={"name"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"PLACE AND DATE OF BIRTH"}
-                            name={"birthday"}
-                            type={"date"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"PLACE AND DATE OF BIRTH"}
+                                    name={"birthday"}
+                                    type={"date"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"COUNTRY OF ORIGIN"}
-                            name={"country"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"COUNTRY OF ORIGIN"}
+                                    name={"country"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"IDENTITY CARD"}
-                            name={"identity"}
-                            type={"number"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"IDENTITY CARD"}
+                                    name={"identity"}
+                                    type={"number"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"IDENTITY CARD NUMBER"}
-                            name={"identity_number"}
-                            type={"number"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"IDENTITY CARD NUMBER"}
+                                    name={"identity_number"}
+                                    type={"number"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"EXPIRATION DATE"}
-                            name={"expiration_date"}
-                            type={"date"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"EXPIRATION DATE"}
+                                    name={"expiration_date"}
+                                    type={"date"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"FACULTY OBJECTIVES"}
-                            name={"faculty_objectives"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"FACULTY OBJECTIVES"}
+                                    name={"faculty_objectives"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"STUDY PROGRAM"}
-                            name={"study_program"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"STUDY PROGRAM"}
+                                    name={"study_program"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"PHONE NUMBER/WA"}
-                            name={"phone"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"PHONE NUMBER/WA"}
+                                    name={"phone"}
+                                    required={true}
+                                />
+                        </div>
 
-                        <FormInput
-                            control={form.control}
-                            label={"Academic transcripts and high school / MK,  diploma, or equivalent"}
-                            name={"transcript"}
-                            type={"file"}
-                            required={true}
-                        />
+                        <h2 className={"my-8 font-bold text-2xl"}>CRITERIA AND REQUIREMENTS</h2>
+                        <div className={"grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-2"}>
 
-                        <FormInput
-                            control={form.control}
-                            label={"Certificate of academic achievement and/or non-academic achievement (if Any)"}
-                            name={"certificate"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Academic transcripts and high school / MK,  diploma, or equivalent"}
+                                    name={"transcript"}
+                                    type={"file"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"Maximum age 35 years (Proved by Birth Certificate)"}
-                            name={"certificate_birthday"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Certificate of academic achievement and/or non-academic achievement (if Any)"}
+                                    name={"certificate"}
+                                    type={"file"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"Motivation Letter (Ms. Word or PDF)"}
-                            name={"motivation_letter"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Maximum age 35 years (Proved by Birth Certificate)"}
+                                    name={"certificate_birthday"}
+                                    type={"file"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"Passport (min. 30 months after entering college)"}
-                            name={"passport"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Motivation Letter (Ms. Word or PDF)"}
+                                    name={"motivation_letter"}
+                                    type={"file"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"Medical certificate"}
-                            name={"medical_certificate"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Passport (min. 30 months after entering college)"}
+                                    name={"passport"}
+                                    type={"file"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"Passport-sized photo"}
-                            name={"passport_photo"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Medical certificate"}
+                                    name={"medical_certificate"}
+                                    type={"file"}
+                                    required={true}
+                                />
 
-                        <FormInput
-                            control={form.control}
-                            label={"English Proficiency Certificate: TOEFL/TOEIC/IELTS or other valid certificate)"}
-                            name={"english_certificate"}
-                            type={"file"}
-                            required={true}
-                        />
+                                <FormInput
+                                    control={form.control}
+                                    label={"Passport-sized photo"}
+                                    name={"passport_photo"}
+                                    type={"file"}
+                                    required={true}
+                                />
+
+                                <FormInput
+                                    control={form.control}
+                                    label={"English Proficiency Certificate: TOEFL/TOEIC/IELTS or other valid certificate)"}
+                                    name={"english_certificate"}
+                                    type={"file"}
+                                    required={true}
+                                />
                         </div>
                         <Button type={"submit"} className={"mt-4 w-24 text-white"}>Submit</Button>
                     </form>
