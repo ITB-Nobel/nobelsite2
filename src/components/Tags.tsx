f8a12cf53e0c0c42444b711e7f19e7c0e6d8ed1b
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-03-16 05:55:36 +0800

    style: tags component hover

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -14,7 +14,7 @@ const Tags = ({key, ids, className}: TagsType) => {
     const {data} =
         useSWR<Tags[]>(`${key}-${idsString}`, () => fetcher<Tags[]>(`tags?include=${idsString}`))
     if (data && ids?.length > 0)
-        return <> {data?.map((item, index) => <div className={`${className} hover:text-white capitalize hover:bg-black cursor-pointer`} key={index}>{item.name}</div>)} </>
+        return <> {data?.map((item, index) => <div className={`${className} hover:text-white capitalize hover:bg-black cursor-pointer px-2 py-1 rounded-full`} key={index}>{item.name}</div>)} </>
 
 }
 export default Tags
