c5af0977fc70c03537ea18ffcfec42cefdacd961
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-05-03 06:44:57 +0800

    style: change image dashboard

--- src/components/BlurImage.tsx
+++ src/components/BlurImage.tsx
@@ -26,3 +26,30 @@ export default function BlurImage({image, alt, className}: { image: string, alt:
 
     );
 }
+
+
+export function BlurImage2({image, alt, className}: { image: string, alt: string, className?: string }) {
+    const [isLoading, setLoading] = useState(true);
+
+    return (
+
+        <div
+            className={`${className} aspect-w-2 aspect-h-1  overflow-hidden  bg-gray-200 `}>
+            <Image
+                alt={alt}
+                src={image}
+                layout="fill"
+                objectFit="fill"
+                className={`
+              duration-700 ease-in-out group-hover:opacity-75
+              ${
+                    isLoading
+                        ? "scale-110 blur-2xl grayscale"
+                        : "scale-100 blur-0 grayscale-0"
+                })`}
+                onLoadingComplete={() => setLoading(false)}
+            />
+        </div>
+
+    );
+}
