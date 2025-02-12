d4e2b098a4b517eb988b8e1ac25e03ff8188bc0b
sofyan <sofyanardiansyah888@gmail.com>
2025-02-12 18:30:06 +0800

    feat: add search and filter

--- src/app/publikasi/page.tsx
+++ src/app/publikasi/page.tsx
@@ -1,15 +1,48 @@
 'use client'
-import React from "react";
+import React, {useState} from "react";
 import GeneralLayout from "@/components/layout/GeneralLayout";
 import useSWR from "swr";
 import {Dosen} from "@/lib/types";
 import {fetcherAcf} from "@/lib/api";
 import {MoveRight, SearchIcon} from "lucide-react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue
+} from "@/components/Select";
 
 export default function KutipanPage() {
+    const [search, setSearch] = useState("")
+    const [prodi, setProdi] = useState("")
     const {data} = useSWR<{ acf: Dosen }[]>(`dosenx`, () => fetcherAcf<{
         acf: Dosen
-    }[]>('dosen?per_page=500'))
+    }[]>('dosen?per_page=1000'))
+
+    function formatDepartmentName(input: string): string {
+        return input.replace(/^\d+/, "") // Hapus angka di awal
+            .replace(/-/g, " ") // Ganti "-" dengan spasi
+            .replace(/\b\w/g, c => c.toUpperCase()); // Kapitalisasi setiap kata
+    }
+    const filteredData = data?.filter((item) => {
+        const nameMatches = item.acf.name.toLowerCase().includes(search.toLowerCase());
+
+        const selectedProdi = item?.acf?.prodi?.find(
+            (prod) => prod.post_title?.toLowerCase() === formatDepartmentName(prodi)?.toLowerCase()
+        );
+
+        // Jika `prodi` dipilih dan bukan string kosong, filter berdasarkan prodi
+        if (prodi && prodi.trim() !== "") {
+            return selectedProdi && nameMatches;
+        }
+
+        // Jika hanya cari berdasarkan nama
+        return nameMatches;
+    });
+
     return <GeneralLayout
         featuredTitle={"Kutipan"}
         // featuredTitle={tempData?.title?.rendered}
@@ -20,16 +53,42 @@ export default function KutipanPage() {
                 <h1>Publication</h1>
                 <p>Panduan Sitasi (Manajemen Referensi)</p>
             </div>
-            <div className={"w-full flex justify-end"}>
+            <div className={"w-full flex gap-2 justify-end"}>
+                <Select onValueChange={(value)=> {
+                    setProdi(value)
+                }}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Pilih prodi" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Pilih Prodi</SelectLabel>
+                            <SelectItem value=" ">All</SelectItem>
+                            <SelectItem value="akuntansi">Akuntansi</SelectItem>
+                            <SelectItem value="manajemen">Manajemen</SelectItem>
+                            <SelectItem value="sistem dan teknologi informasi">Sistem dan Teknologi Informasi</SelectItem>
+                            <SelectItem value="teknik pertambangan">Teknik Pertambangan</SelectItem>
+                            <SelectItem value="teknologi hasil perikanan">Teknologi Hasil Perikanan</SelectItem>
+                            <SelectItem value="teknologi industri">Teknologi Industri</SelectItem>
+                            <SelectItem value="pendidikan teknologi informasi">Pendidikan Teknologi Informasi</SelectItem>
+                            <SelectItem value="magister manajemen">Magister Manajemen</SelectItem>
+                            <SelectItem value="magister keuangan publik">Magister Keuangan Publik</SelectItem>
+                            <SelectItem value="magister manajemen dan kewirausahaan">Magister Manajemen dan Kewirausahaan</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
                 <div className="flex">
                     <input type="text" id="website-admin"
                            className="rounded-none rounded-l-lg  border border-gray-300  block flex-1 min-w-0 w-full text-sm p-2.5  "
-                           placeholder="Search"/>
+                           placeholder="Search"
+                           onChange={(e) => setSearch(e.target.value)}
+                    />
                     <span
                         className="inline-flex items-center bg-primary text-white px-3 text-sm border border-e-0 border-gray-300 rounded-r-md">
                         <SearchIcon/>
                     </span>
                 </div>
+
             </div>
 
 
@@ -46,30 +105,38 @@ export default function KutipanPage() {
                     </tr>
                     </thead>
                     <tbody className="">
-                    {data &&
-                        data.map((item, index) => (
+                    {filteredData &&
+                        filteredData.map((item, index) => (
                             <tr key={index} className="odd:bg-white even:bg-slate-50">
                                 <td className="px-6 py-2">{item.acf.name}</td>
-                                <td className="px-6 py-2">{item.acf.name}</td>
+                                <td className="px-6 py-2">{item.acf.prodi.map((item) => formatDepartmentName(item.post_name))}</td>
                                 <td className="px-6 py-2">
-                                    <a
-                                        href={![null, undefined, ''].includes(item.acf.google_scholar) ? item.acf?.google_scholar as string : "#"}
-                                        onClick={(e) => e.preventDefault()}
+                                    <button
+                                        className={"px-4 py-2 bg-green-300 text-white w-fit rounded-full disabled:bg-slate-300"}
+                                        disabled={[null, undefined, ''].includes(item.acf.google_scholar)}
+                                        onClick={() => {
+                                            if (![null, undefined, ''].includes(item.acf.google_scholar))
+                                                window.open(item.acf?.google_scholar as string, '_blank', 'noopener,noreferrer')
+                                        }}
                                     >
-                                        <div className={"px-4 py-2 bg-green-300 text-white w-fit rounded-full "}>
+                                        <div className={""}>
                                             <MoveRight/>
                                         </div>
-                                    </a>
+                                    </button>
                                 </td>
                                 <td className="px-6 py-2">
-                                    <a
-                                        href={![null, undefined, ''].includes(item.acf.scoopus) ? item.acf?.scoopus as string : "#"}
-                                        onClick={(e) => e.preventDefault()}
+                                    <button
+                                        className={"px-4 py-2 bg-green-300 text-white w-fit rounded-full disabled:bg-slate-300"}
+                                        disabled={[null, undefined, ''].includes(item.acf.scoopus)}
+                                        onClick={() => {
+                                            if (![null, undefined, ''].includes(item.acf.scoopus))
+                                                window.open(item.acf?.scoopus as string, '_blank', 'noopener,noreferrer')
+                                        }}
                                     >
-                                        <div className={"px-4 py-2 bg-green-300 text-white w-fit rounded-full "}>
+                                        <div className={" "}>
                                             <MoveRight/>
                                         </div>
-                                    </a>
+                                    </button>
                                 </td>
                             </tr>
                         ))}
