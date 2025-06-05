f5186af1a95d46f86abc444f4c5abe4f8870e5a0
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2025-06-05 09:46:40 +0800

    Perbaikan Metada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,33 @@ import Head from "next/head";
 // const localFont1 = localFont({ src: '../fonts/gt-america-standard-medium.woff' })
 
 export const metadata: Metadata = {
-    title: 'Nobel Institute',
-    description: 'Start Here Go Anywhere',
+    title: 'Nobel Indonesia Institute',
+    description: 'Fakultas Teknologi dan Bisnis, Fakultas Pascasarjana : Start Here Go Anywhere',
+    keywords: 'Fakultas Teknologi dan Bisnis : Manajemen, Akuntansi, Sistem dan Teknologi Informasi, Teknik Pertambangan, Teknologi Hasil Perikanan,Teknik Industri, Pendidikan Teknologi Informasi, Fakultas Pascasarjana : Magister Manajemen, Magister Keuangan Publik, Magister Manajemen dan Kewirausahaan',
+    openGraph: {
+        type: 'website',
+        url: 'https://nobel.ac.id/',
+        title: 'Nobel Indonesia Institute',
+        
+        description: 'Fakultas Teknologi dan Bisnis, Fakultas Pascasarjana : Start Here Go Anywhere',
+        images: [
+            {
+                url: '/images/logo.png',
+                width: 800,
+                height: 600,
+                alt: 'Nobel Indonesia Institute'
+            }
+        ],
+        siteName: 'Nobel Indonesia Institute'
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Nobel Indonesia Institute',
+        description: 'Empowering Future Generations Through Quality Education and Innovation',
+        images: ['/images/logo.png']
+    }
 }
+
 
 export default function RootLayout({
                                        children,
